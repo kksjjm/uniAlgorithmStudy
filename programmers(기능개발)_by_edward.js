05bb2208b62748c360a3886e1e852d09b43eac31
injoong98 <68066289+injoong98@users.noreply.github.com>
2020-07-15 17:49:30 +0900

    기능개발 수정

--- programmers(기능개발)_by_edward.js
+++ programmers(기능개발)_by_edward.js
@@ -7,16 +7,13 @@ function solution(progresses, speeds) {
     
     for(var i=0; i<days.length; i++){
         count++;
-        if(i==days.length-1){
-            answer.push(count);
-            break;
-        }
-        else if(days[i+1]>max){
+        if(days[i+1]>max){
             answer.push(count);
             max = days[i+1];
             count = 0;
         }
     }
+    answer.push(count);
     
     return answer;
 }
